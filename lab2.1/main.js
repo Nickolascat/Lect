f97fb1be0291ff6dc7c95ace658b1160bf31f17d
Nickolascat <75448598+Nickolascat@users.noreply.github.com>
2021-03-10 20:57:16 +0200

    Update main.js

--- lab2.1/main.js
+++ lab2.1/main.js
@@ -1,36 +1,7 @@
-const min = require('./min');
-const summ = require('./summ');
-const abs = require('./abs');
+const min = require('./min.js');
+const summ = require('./summ.js');
+const abs = require('./abs.js');
 
 console.log(min([-5, 4, 3, -25, 13]));
 console.log(summ([4, 3, -2, 1]));
 console.log(abs(-16));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
